13edd925836f7db30ce6107b105728f09b9c1cc7
Carlos Rodrigues <contato@carlosr.dev>
2021-10-23 20:38:47 -0300

    Add sonar cloud to pipeline

--- src/math/operations.js
+++ src/math/operations.js
@@ -1,11 +1,28 @@
 const operations = {
-    sum: (a, b) => {
+    add: (a, b) => {
         return a + b;
     },
 
-    minus: (a, b) => {
+    sub: (a, b) => {
         return a - b;
+    },
+
+    mult: (a, b) => {
+        return a * b;
+    },
+
+    div: (a, b) => {
+        return a/b;
+    },
+
+    pow: (a, b) => {
+        return Math.pow(a, b);
+    },
+
+    sqrt: (a) => {
+        return Math.sqrt(a)
     }
+
 }
 
 module.exports = operations;
